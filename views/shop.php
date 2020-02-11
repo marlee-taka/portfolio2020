6ac8da074247dedec00a5249fbb04c17db921ff1
Kirby <kirbpao@gmail.com>
2020-02-11 17:49:35 +0800

    Updated shop.php

--- views/shop.php
+++ views/shop.php
@@ -1,4 +1,69 @@
-<?php
-    //HI Iwa-chan!
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/shop.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 2-">
+                <img src="../assets/img/fog-pics/fog-tee.jpg" alt="FOG-tee" class="ml-5" style="width: 80%;">
+            </div>
+            <div class="col-md-6 mt-5">
+                <h2 class="display-4 mt-5">SHORT SLEEVE '3M' TEE</h2>
+                <form action="" method="post">
+                    <div class="form-row border-bottom mt-5">
+                        <div class="col-md-6">
+                            <h3>Size</h3>
+                        </div>
+                        <div class="col-md-6">
+                            <select name="size" id="" class="form-control shopform border border-0">
+                                <!-- <option value="#" selected disabled>SIZE</option> -->
+                                <option value="x-small">XS</option>
+                                <option value="small">S</option>
+                                <option value="medium">M</option>
+                                <option value="large">L</option>
+                                <option value="x-large">XL</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-row border-bottom mt-5">
+                        <div class="col-md-6">
+                            <h3>Color</h3>
+                        </div>
+                        <div class="col-md-6">
+                            <select name="color" id="" class="form-control shopform border border-0">
+                                <!-- <option value="#" selected disabled>SIZE</option> -->
+                                <option value="white">White</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-row border-bottom mt-5">
+                        <div class="col-md-6">
+                            <h3>Quantity</h3>
+                        </div>
+                        <div class="col-md-6">
+                            <select name="quantity" id="" class="form-control shopform border border-0">
+                                <!-- <option value="#" selected disabled>SIZE</option> -->
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
